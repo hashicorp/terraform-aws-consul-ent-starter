2816c7719b62cb2e54c21fae9b96cf0ce0b14174
Omar Khawaja <khawaja910@gmail.com>
2022-06-29 09:45:54 -0400

    remove subnet id output from main module outputs

--- outputs.tf
+++ outputs.tf
@@ -3,11 +3,6 @@ output "consul_client_sg_id" {
   value       = module.vm_servers.consul_clients_sg_id
 }
 
-output "consul_subnet_ids" {
-  description = "AWS subnets ID to deploy Consul clients"
-  value       = module.networking.consul_subnet_ids
-}
-
 output "kms_key_arn_s3_bucket" {
   description = "KMS Key ARN used for S3 bucket encryption. Any clients being provisioned will need to access this for the Consul Enterprise license"
   value       = module.object_storage.kms_key_arn
